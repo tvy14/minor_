4c26c4a94629d9f4ff1b215675b538c3aae0bc2e
tvy14 <timovy.ee11@nycu.edu.tw>
2024-06-03 11:34:11 +0800

    erase unnecessary lines

--- fronthaul_linkbudget.py
+++ fronthaul_linkbudget.py
@@ -30,37 +30,3 @@ class FronthaulOptim:
     throughput_mbps = ###
 
     return throughput_tps, throughput_mbps
-
-def plot_results(results):
-    plt.figure(figsize=(10, 6))
-
-    plt.subplot(2, 2, 1)
-    plt.plot([result['num_nodes'] for result in results], [result['throughput_tps'] for result in results], label='PBFT')
-    plt.xlabel('Number of nodes')
-    plt.ylabel('Throughput (TPS)')
-    plt.title('PBFT Throughput vs. Number of Nodes')
-
-    plt.subplot(2, 2, 2)
-    plt.plot([result['num_nodes'] for result in results], [result['throughput_mbps'] for result in results], label='PBFT')
-    plt.xlabel('Number of nodes')
-    plt.ylabel('Throughput (Mbps)')
-    plt.title('PBFT Throughput (Mbps) vs. Number of Nodes')
-
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
-
-if __name__ == '__main__':
-    num_transactions = 10000
-
-    results = []
-    for num_nodes in [10, 50, 100, 1000]:
-        throughput_tps, throughput_mbps = simulate(num_nodes, num_transactions)
-
-        results.append({
-            'num_nodes': num_nodes,
-            'throughput_tps': throughput_tps,
-            'throughput_mbps': throughput_mbps
-        })
-
-    plot_results(results)
